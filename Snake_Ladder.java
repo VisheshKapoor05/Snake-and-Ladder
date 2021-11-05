90ca063e6b77667099ae15215906282bf5b339d2
Vishesh Kapoor <vishesh17kapoor@gmail.com>
2021-11-05 12:18:49 +0530

    Player throws Dice

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -3,6 +3,10 @@ public class Snake_Ladder {
 public static void main(String[] args) {
 System.out.println("Snake and ladder game");
 int pos = 0;
+
+double DiceNum = (Math.floor(Math.random()*10)%6)+1;
+System.out.println(DiceNum);
+
 }
 
 }
