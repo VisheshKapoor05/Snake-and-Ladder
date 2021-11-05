1e61d8a0284f7c9464c70126ac8651d21cdea968
Vishesh Kapoor <vishesh17kapoor@gmail.com>
2021-11-05 12:45:59 +0530

    Player checks for options: Noplay, Ladder or Snake

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -2,10 +2,32 @@ public class Snake_Ladder {
 
 public static void main(String[] args) {
 System.out.println("Snake and ladder game");
-int pos = 0;
+double pos = 0;
 
 double DiceNum = (Math.floor(Math.random()*10)%6)+1;
 System.out.println(DiceNum);
+
+double Option = Math.floor(Math.random()*10)%3;
+
+if(Option == 0)
+	pos = pos;
+else if(Option == 1)
+	pos = pos + DiceNum ;
+else
+	pos = pos - DiceNum ;
+
+/*switch ((int) Option) {
+	case 0:
+		pos = pos; //NoPlay
+	case 1:
+		pos = pos + DiceNum ; //Ladder
+	case 2:
+		pos = pos - DiceNum ; //Snake
+	}
+*/
+
+System.out.println(Option);
+System.out.println(pos);
 
 }
 
