d8c18d2a084589e1af2f3b353db0c7183bf016aa
Vishesh Kapoor <vishesh17kapoor@gmail.com>
2021-11-05 13:16:35 +0530

    Player crosses the winning position 100

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -1,21 +1,37 @@
-public class Snake_Ladder {
+public class Snake_Ladder
+{
 
-public static void main(String[] args) {
+public static void main(String[] args)
+{
 System.out.println("Snake and ladder game");
 double pos = 0;
 
+int a=0;
+while(pos<=100)
+{
 double DiceNum = (Math.floor(Math.random()*10)%6)+1;
-System.out.println(DiceNum);
+System.out.println("DiceNum" +DiceNum);
 
 double Option = Math.floor(Math.random()*10)%3;
 
-if(Option == 0)
-	pos = pos;
-else if(Option == 1)
-	pos = pos + DiceNum ;
-else
-	pos = pos - DiceNum ;
+	if(Option == 0)            //NoPlay
+		pos = pos;
+	else if(Option == 1)       //Ladder
+		pos = pos + DiceNum ;
+	else                       //Snake
+		{
+			pos = pos - DiceNum ;
+			if(pos<0)
+			{
+				pos=0;
+			}
+		}
 
+System.out.println("Option" +Option);
+System.out.println("position" +pos);
+System.out.println();
+
+}
 /*switch ((int) Option) {
 	case 0:
 		pos = pos; //NoPlay
@@ -25,8 +41,6 @@ else
 		pos = pos - DiceNum ; //Snake
 	}
 */
-
-System.out.println(Option);
 System.out.println(pos);
 
 }
