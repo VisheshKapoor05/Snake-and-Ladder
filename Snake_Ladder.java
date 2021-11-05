af04f3b0de0e01a8b768bd7da68022c765d5bd66
Vishesh Kapoor <vishesh17kapoor@gmail.com>
2021-11-05 15:30:02 +0530

    added switch cases

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -8,23 +8,26 @@ double DiceNum = (Math.floor(Math.random()*10)%6)+1;
 System.out.println(DiceNum);
 
 double Option = Math.floor(Math.random()*10)%3;
-
+/*
 if(Option == 0)
 	pos = pos;
 else if(Option == 1)
 	pos = pos + DiceNum ;
 else
 	pos = pos - DiceNum ;
-
-/*switch ((int) Option) {
+*/
+switch ((int) Option) {
 	case 0:
 		pos = pos; //NoPlay
+		break;
 	case 1:
 		pos = pos + DiceNum ; //Ladder
+		break;
 	case 2:
 		pos = pos - DiceNum ; //Snake
+		break;
 	}
-*/
+
 
 System.out.println(Option);
 System.out.println(pos);
